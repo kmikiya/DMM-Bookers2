4dbfc8ab9b9e1e492ba11ff3342414dd71d95013
EC2 Default User <ec2-user@ip-172-31-88-206.ec2.internal>
2021-10-11 07:24:51 +0000

    4th

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
       @books = Book.all
       @book = Book.new
       @user = current_user
+
   end
 
   def create
@@ -17,14 +18,26 @@ class BooksController < ApplicationController
       @book_show = Book.find(params[:id])
       @user_show = @book_show.user
       @user = User.find(params[:id])
+      @user_current = current_user#部分テンプレートでつかってる
      # @user = User.find(params[:id])
       @book = Book.new
+      @user_edit = current_user#16行目のif文で使ってる
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key =
+'1ab17db316d058ed060e092be0b09db719bfca8478515ab2de709fbe6ecfca690d4910d7c415f84ee46388fb765522decc22d5baf3c5e97e806c804d2f48871b'
